7fedc54cab0b0603adcccfcc0009e48da5b02f5f
Chris Alfano <chris@jarv.us>
2019-11-21 21:21:41 +0000

    fix: habitize bin script interpretters

--- plan.sh
+++ plan.sh
@@ -38,14 +38,15 @@ do_build() {
 }
 
 do_install() {
-  build_line "Creating command wrappers"
-
+  build_line "Generating emergence-php-exec wrapper"
   cat > "${pkg_prefix}/bin/emergence-php-exec" <<- EOM
 #!/bin/bash
 exec ${pkg_svc_config_path}/fpm-exec \$@
 EOM
 
+  build_line "Installing bin commands"
   cp -v "${PLAN_CONTEXT}/bin"/* "${pkg_prefix}/bin/"
+  fix_interpreter "${pkg_prefix}/bin/*" core/bash bin/bash
 
   chmod +x "${pkg_prefix}/bin"/*
 }

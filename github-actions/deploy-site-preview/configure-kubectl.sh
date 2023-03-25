18039855e0fd7896b79a3fbc0fe6e8505b7e6854
Chris Alfano <chris@jarv.us>
2023-03-25 15:24:34 -0400

    feat(actions): configure and output k8s namespace

--- github-actions/deploy-site-preview/configure-kubectl.sh
+++ github-actions/deploy-site-preview/configure-kubectl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# write KUBECONFIG
 test -e ~/.kube || mkdir ~/.kube
 printf '%s' "$KUBE_CONFIG_DATA" | base64 -d >~/.kube/config
+
+# switch to configured namespace
+kubectl config set-context --current --namespace="${KUBE_NAMESPACE}"
+
+echo "Outputting: kube-namespace=${IMAGE_TAG_FULL}"
+echo "kube-namespace=${KUBE_NAMESPACE}" >>"${GITHUB_OUTPUT}"
